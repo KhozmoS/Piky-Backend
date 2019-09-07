44595921e6e7216e6a3b3ceff5cf0429d23c7770
KhozmoS <khozmos0107@gmail.com>
2019-09-07 10:14:46 -0700

    Added Cors policy  :police_car:

--- Startup.cs
+++ Startup.cs
@@ -26,6 +26,7 @@ namespace PikyServer
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,7 +41,11 @@ namespace PikyServer
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseCors( builder => 
+                builder.WithOrigins("http://localhost:4200")
+                .WithMethods("GET","POST","PUT","DELETE")
+                .AllowAnyHeader()
+            );
             app.UseHttpsRedirection();
             app.UseMvc();
         }
